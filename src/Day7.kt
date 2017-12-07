75fed4df14b52d82c62351c23a84ba3642505ed5
ktarasenko <konstantin.v.tarasenko@gmail.com>
2017-12-07 09:09:49 +0100

    day7 wip

--- src/Day7.kt
+++ src/Day7.kt
@@ -1,7 +1,41 @@
+import javafx.scene.text.FontWeight
+
 object Day7 {
 
-    fun solve(list: Iterable<String>): Int {
+    fun solve(list: List<String>): String =
+            findRoot(list.map { parse(it) })
+
+    private fun findRoot(list: List<Disk>): String =
+            list.fold(emptySet<String>() to emptySet<String>(), { acc, it ->
+                acc.first.plus(it.name) to acc.second.plus(it.children)
+            }).run { first.minus(second).first() }
+
+
+    private fun parse(l: String): Disk {
+        // fwft (72) -> ktlj, cntj, xhth
+        val ls = l.split("->")
+        val nw = ls.first()
+        val c = ls.getOrElse(1, { "" })
+        val (name, wS) = nw.split(" ")
+        val children = c.split(",").filter { it.isNotBlank() }
+        return Disk(name, wS.drop(1).dropLast(1).toInt(), children)
+    }
+
+
+    data class Disk(val name: String, val weight: Int, val children: List<String>)
+
+    fun solve2(list: List<String>): Long {
+        val disks = list.map { parse(it) }
+        val disksMap = disks.map { it.name to it }.toMap()
+        val rootNode = disksMap.getValue(solve(list))
+        rootNode.children.map { subtreeSum(it, disksMap)}
         return 42
+    }
+
+    private fun subtreeSum(node: String, disksMap: Map<String, Day7.Disk>): Int {
+        return disksMap.getValue(node).run{
+            weight + children.sumBy { subtreeSum(it, disksMap) }
+        }.also { println("$it, $node") }
     }
 
 }
--- test/Day7Test.kt
+++ test/Day7Test.kt
@@ -6,12 +6,36 @@ class Day7Test {
 
     @Test
     fun provided(){
-        Assert.assertEquals(42, Day7.solve(("a\nb").split("\n")))
+        Assert.assertEquals("tknk", Day7.solve(("pbga (66)\n" +
+                "xhth (57)\n" +
+                "ebii (61)\n" +
+                "havc (66)\n" +
+                "ktlj (57)\n" +
+                "fwft (72) -> ktlj, cntj, xhth\n" +
+                "qoyq (66)\n" +
+                "padx (45) -> pbga, havc, qoyq\n" +
+                "tknk (41) -> ugml, padx, fwft\n" +
+                "jptl (61)\n" +
+                "ugml (68) -> gyxo, ebii, jptl\n" +
+                "gyxo (61)\n" +
+                "cntj (57)").split("\n")))
     }
 
     @Test
     fun provided2(){
-        Assert.assertEquals(42, Day7.solve(("a\nb").split("\n")))
+        Assert.assertEquals(8, Day7.solve2(("pbga (66)\n" +
+                "xhth (57)\n" +
+                "ebii (61)\n" +
+                "havc (66)\n" +
+                "ktlj (57)\n" +
+                "fwft (72) -> ktlj, cntj, xhth\n" +
+                "qoyq (66)\n" +
+                "padx (45) -> pbga, havc, qoyq\n" +
+                "tknk (41) -> ugml, padx, fwft\n" +
+                "jptl (61)\n" +
+                "ugml (68) -> gyxo, ebii, jptl\n" +
+                "gyxo (61)\n" +
+                "cntj (57)").split("\n")))
     }
 
     @Test
@@ -21,11 +45,11 @@ class Day7Test {
 
     @Test
     fun task(){
-        Assert.assertEquals(42, Day7.solve((File("input/day7.txt").readLines())))
+        Assert.assertEquals("cqmvs", Day7.solve((File("input/day7.txt").readLines())))
     }
 
     @Test
     fun task2(){
-        Assert.assertEquals(42, Day7.solve((File("input/day7.txt").readLines())))
+        Assert.assertEquals("cqmvs", Day7.solve2((File("input/day7.txt").readLines())))
     }
 }
