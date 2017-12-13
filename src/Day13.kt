d217461333797615d887bcc04da6b40d32d24b45
ktarasenko <konstantin.v.tarasenko@gmail.com>
2017-12-13 09:17:14 +0100

    day13 naive solution

--- src/Day13.kt
+++ src/Day13.kt
@@ -1,11 +1,30 @@
 object Day13 {
 
-    fun solve(list: Iterable<String>): Int {
-        return 42
-    }
+    fun solve(list: Iterable<String>): Int =
+        severity(0, parse(list))
+
+    private fun parse(list: Iterable<String>): List<Pair<Int, Int>> =
+        list.map {
+            val (layer, length) = it.split(": ")
+            layer.toInt() to length.toInt()
+        }
+
+
+    private fun severity(delay: Int, list: List<Pair<Int, Int>>): Int =
+        list.fold(0, {acc, (layer, length) ->
+            if ((delay+layer)% (2*length-2) == 0){
+                acc + layer*length
+            } else {
+                acc
+            }
+        })
+
 
     fun solve2(list: Iterable<String>): Int {
-        return 42
+        val firewall = parse(list)
+        return (0..Int.MAX_VALUE).first { delay ->
+            firewall.none { (layer, length) -> (delay + layer) % (2 * length - 2) == 0 }
+        }
     }
 
 }
--- test/Day13Test.kt
+++ test/Day13Test.kt
@@ -7,30 +7,35 @@ class Day13Test {
 
     @Test
     fun provided(){
-        Assert.assertEquals(42, Day13.solve(("a\nb").split("\n")))
+        Assert.assertEquals(24, Day13.solve(("0: 3\n" +
+                "1: 2\n" +
+                "4: 4\n" +
+                "6: 4").split("\n")))
     }
 
     @Test
-    @Ignore
     fun provided2(){
-        Assert.assertEquals(42, Day13.solve2(("a\nb").split("\n")))
+        Assert.assertEquals(10, Day13.solve2(("0: 3\n" +
+                "1: 2\n" +
+                "4: 4\n" +
+                "6: 4").split("\n")))
     }
 
     @Test
-    @Ignore
     fun test(){
-        Assert.assertEquals(42, Day13.solve(("a\nb").split("\n")))
+        Assert.assertEquals(12*4, Day13.solve(("12: 4").split("\n")))
+        Assert.assertEquals(0, Day13.solve(("1: 2").split("\n")))
+        Assert.assertEquals(8*2, Day13.solve(("8: 2").split("\n")))
+        Assert.assertEquals(8*3, Day13.solve(("8: 3").split("\n")))
     }
 
     @Test
-    @Ignore
     fun task(){
-        Assert.assertEquals(42, Day13.solve((File("input/day13.txt").readLines())))
+        Assert.assertEquals(2264, Day13.solve((File("input/day13.txt").readLines())))
     }
 
     @Test
-    @Ignore
     fun task2(){
-        Assert.assertEquals(42, Day13.solve2((File("input/day13.txt").readLines())))
+        Assert.assertEquals(3875838, Day13.solve2((File("input/day13.txt").readLines())))
     }
 }
